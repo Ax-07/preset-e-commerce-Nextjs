87f6388b029cf4e1a34da33bf1f2ec52390f5546
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-19 10:33:25 +0200

    feat: ajouter le composant Hero avec un carrousel d'images et des boutons d'action

---
+++ src/blocks/Hero/HeroCarousel.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/src/components/ui/carousel";
+
+const pictures = [
+  "https://plus.unsplash.com/premium_photo-1724593825200-39731dcdacf8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1682629430351-2cbf66cff813?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDh8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1743865422216-d8a188311ab9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://plus.unsplash.com/premium_photo-1716832318033-30fe0671866b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1744273556970-d5cff25b2b4a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1744132813635-128ed5004f65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1743856842985-e1d4fc72a255?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1743660624046-8e914498fb4d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1743485753906-f190eb90e300?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ5fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/photo-1697172812290-6543a011c3ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDY0fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+];
+
+const HeroCarousel: React.FC = () => {
+  return (
+    <Carousel className="w-full" loop itemsToShow={3} autoplay={5000}>
+      <CarouselContent>
+        {pictures.map((src, index) => (
+          <CarouselItem key={index}>
+            <img
+              src={src}
+              alt={`Image ${index + 1}`}
+              className="w-full aspect-square object-cover"
+              loading="lazy"
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      {/* <CarouselPrevious /> */}
+      {/* <CarouselNext /> */}
+    </Carousel>
+  );
+};
+
+export default HeroCarousel;
