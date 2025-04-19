d7f1340f3d53efe1b50d35e103754cf38847388e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-19 10:31:49 +0200

    feat: ajouter la page des promotions avec contenu descriptif

---
+++ app/(client)/promotions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PagePromotions = () => {
+    return (
+        <div>
+            <h1>Promotions</h1>
+            <p>Découvrez nos meilleures offres et réductions sur une sélection de produits.</p>
+        </div>
+    );
+};
+
+export default PagePromotions;
