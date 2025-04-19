38635d9576fb9df467f0654d059fe28c5fc8cc4b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-19 10:31:54 +0200

    feat: ajouter la page de support client avec contenu descriptif

---
+++ app/(client)/support/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageSupport = () => {
+    return (
+        <div>
+            <h1 className="mb-4 text-2xl font-bold">Support Client</h1>
+            <p className="mb-4">Nous sommes là pour vous aider !</p>
+        </div>
+    );
+};
+
+export default PageSupport;
