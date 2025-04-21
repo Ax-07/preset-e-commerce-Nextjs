53593bf88fc4d0449b0d4994ae15730431bb28db
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-21 09:37:20 +0200

    feat: ajouter la fonctionnalité de tri des produits sur la page des produits

--- app/(client)/produits/page.tsx
+++ app/(client)/produits/page.tsx
@@ -1,14 +1,47 @@
+"use client";
 // @/src/app/produits/page.tsx
 
 import { List, ListItem } from "@/src/blocks/products/List";
 import ProductCard from "@/src/blocks/products/ProductCard";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { PRODUCTS } from "@/products.exemple";
 import { Separator } from "@/src/components/ui/separator";
 import ProductWithOptionsCard from "@/src/blocks/products/ProductWithOptionsCard";
 import { CBD_FLOWERS } from "@/productsFleursCBD.exemple";
+import SortSelector from "@/src/blocks/sortSelector/SortSelector";
+
+const sortOptions = [
+  "Alphabetique (A → Z)",
+  "Alphabetique (A ← Z)",
+  "Prix croissant",
+  "Prix décroissant",
+];
 
 const PageProduits = () => {
+  const [sortBy, setSortBy] = useState<string>(sortOptions[0]);
+
+  // On memoïse le tri pour ne pas recalculer à chaque render
+  const sortedProducts = useMemo(() => {
+    // Clone l’array pour ne pas muter l’original
+    const arr = [...CBD_FLOWERS];
+    switch (sortBy) {
+      case "Alphabetique (A → Z)":
+        return arr.sort((a, b) =>
+          a.name.localeCompare(b.name, "fr", { sensitivity: "base" })
+        );
+      case "Alphabetique (A ← Z)":
+        return arr.sort((a, b) =>
+          b.name.localeCompare(a.name, "fr", { sensitivity: "base" })
+        );
+      case "Prix croissant":
+        return arr.sort((a, b) => a.price! - b.price!);
+      case "Prix décroissant":
+        return arr.sort((a, b) => b.price! - a.price!);
+      default:
+        return arr;
+    }
+  }, [sortBy]);
+  
   return (
     <section id="contact-page" className="flex">
       <div className="flex-1 max-w-50">
@@ -16,9 +49,12 @@ const PageProduits = () => {
       </div>
       <Separator orientation="vertical" className="mx-4" />
       <div className="flex-1">
-        <h2 className="text-xl font-bold">Tous les Produits</h2>
+        <div className="flex justify-between">
+          <h2 className="text-xl font-bold">Tous les Produits</h2>
+          <SortSelector options={sortOptions} onChange={setSortBy}/>
+        </div>
         <List layout="grid" gap="md" cols="4" className="mt-8">
-          {CBD_FLOWERS.map((product) => (
+          {sortedProducts.map((product) => (
             <ListItem key={product.id}>
               {/* <ProductCard product={product} /> */}
               <ProductWithOptionsCard product={product} />
