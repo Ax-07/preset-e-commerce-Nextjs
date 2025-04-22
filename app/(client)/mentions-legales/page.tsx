3a55ba130311abdbb3ab1b8c123a531a13114c49
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-22 09:02:15 +0200

    feat: ajouter la page des mentions légales

---
+++ app/(client)/mentions-legales/page.tsx
@@ -0,0 +1,33 @@
+export default function MentionsLegales() {
+    return (
+      <div className="max-w-3xl mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-4">Mentions légales</h1>
+        <p>Conformément à la loi n°2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique (LCEN), il est précisé aux utilisateurs du site [nom du site] l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+        </p>
+        <p className="mt-4">
+            Éditeur du site : [à compléter] <br />
+            Adresse du siège : [à compléter] <br />
+            Téléphone : [à compléter] <br />
+            Email : exemple@email.com <br />
+            Numéro de SIRET/SIREN : [à compléter] <br />
+            Responsable de la publication : [à compléter] <br />
+            <br />
+            Hébergement : [à compléter] <br />
+            Nom de l’hébergeur : [ex : OVH, Infomaniak, etc.] <br />
+            Adresse de l’hébergeur : [à compléter] <br />
+            Téléphone / Email : [à compléter] <br />
+            <br />
+            Propriété intellectuelle : <br />
+            Le site et son contenu sont protégés par la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation totale ou partielle du site ou de ses éléments sans autorisation écrite préalable de l’éditeur est interdite.<br />
+            <br />
+            Responsabilité : <br />
+            L’éditeur s’efforce de fournir des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des omissions, inexactitudes ou carences dans la mise à jour. <br />
+            <br />
+            ---
+            <br />
+            Ces mentions légales peuvent être modifiées à tout moment.
+        </p>
+      </div>
+    );
+  }
+  
