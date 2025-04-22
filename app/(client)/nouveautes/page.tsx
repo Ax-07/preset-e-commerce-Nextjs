ccfd8244948e87fe9a09a21c63fef43807dad49e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-22 09:02:29 +0200

    feat: ajouter une nouvelle page avec du contenu initial

---
+++ app/(client)/nouveautes/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>Nouvelle page</h1>
+            <p>Voici le contenu de la nouvelle page.</p>
+            <p>Vous pouvez ajouter du texte, des images  </p>
+        </div>
+    );
+};
+export default page;
