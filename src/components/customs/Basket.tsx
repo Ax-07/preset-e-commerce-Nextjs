c47ff14e59387505d0008f07a5d987c014b081c5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-24 07:06:04 +0200

    feat: améliorer le composant Panier avec des boutons de suppression et un style révisé

--- src/components/customs/Basket.tsx
+++ src/components/customs/Basket.tsx
@@ -8,9 +8,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu";
-import { ShoppingBasket } from "lucide-react";
+import { ShoppingBasket, X } from "lucide-react";
 import { useCartStore } from "@/src/stores/cart.store";
 import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 
 const Basket = () => {
   const items = useCartStore(state => state.items);
@@ -27,8 +28,10 @@ const Basket = () => {
           </Badge>
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-48">
-        <DropdownMenuLabel>Mon panier</DropdownMenuLabel>
+      <DropdownMenuContent className="w-100 mr-8">
+        <DropdownMenuLabel>
+          <h4 className="w-fit mx-auto text-base">Mon panier</h4>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
             {items.length === 0 ? (
@@ -38,21 +41,23 @@ const Basket = () => {
             ) : (
               <ul className="space-y-2 mb-4">
                 {items.map(item => (
-                <DropdownMenuItem>
-                  <li key={item.id} className="flex justify-between">
-                    <span>{item.name} x {item.quantity}g</span>
-                    <div>
-                      <span className="mr-2">{(item.unitPrice * item.quantity).toFixed(2)}€</span>
-                      <button
-                        className="text-red-500 hover:underline"
+                <DropdownMenuItem key={item.id}>
+                  <li className="inline-flex items-center justify-between w-full">
+                    <p className="">{item.name} x {item.quantity}g</p>
+                    <div className="inline-flex items-center">
+                      <p className="mr-2">{(item.unitPrice * item.quantity).toFixed(2)}€</p>
+                      <Button
+                        size="icon"
+                        variant="ghost"
+                        className="text-red-500 hover:text-red-500 size-6"
                         onClick={(e) => {
                           e.stopPropagation(); // Empêche la propagation de l'événement de clic
                           e.preventDefault(); // Empêche le comportement par défaut du bouton
                           removeItem(item.id);
                         }}
                       >
-                        Supprimer
-                      </button>
+                        <X className="size-4" />
+                      </Button>
                     </div>
                   </li>
                 </DropdownMenuItem>
@@ -61,14 +66,19 @@ const Basket = () => {
             )}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex justify-between">
+        <div className="flex justify-between px-2 py-1.5">
           <span>Total</span>
           <span>{totalPrice.toFixed(2)}€</span>
-        </DropdownMenuItem>
+        </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <a href="/panier">Voir mon Panier</a>
-        </DropdownMenuItem>
+        <div className="flex flex-col gap-2 px-2 py-1.5">
+          <Button variant="outline" className="w-full" asChild>
+            <a href="/panier">Voir mon Panier</a>
+          </Button>
+          <Button className="w-full" asChild>
+            <a href="/commande">Commander</a>
+          </Button>
+        </div>
       </DropdownMenuContent>
     </DropdownMenu>
   );
