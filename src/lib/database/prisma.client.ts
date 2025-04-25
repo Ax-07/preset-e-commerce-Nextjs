1b61dfec62c139c09fdcb3f0bbb2dcac71bbab44
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-25 08:39:49 +0200

    fix: corriger l'importation du client Prisma dans prisma.client.ts

--- src/lib/database/prisma.client.ts
+++ src/lib/database/prisma.client.ts
@@ -1,10 +1,9 @@
 // lib/prisma.ts
 
-import { PrismaClient } from "@/prisma/generated/prisma/client";
+import { PrismaClient } from "@/prisma/generated/prisma/client/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
