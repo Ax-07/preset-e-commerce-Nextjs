9b02da93b9fc077ea27db1318576c7ec1b273305
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-30 23:05:07 +0200

    fix: remplacer les données des produits par des données mockées et ajouter les catégories de produits

--- app/(client)/produits/page.tsx
+++ app/(client)/produits/page.tsx
@@ -2,13 +2,12 @@
 // @/src/app/produits/page.tsx
 
 import { List, ListItem } from "@/src/blocks/products/List";
-import ProductCard from "@/src/blocks/products/ProductCard";
 import React, { useMemo, useState } from "react";
-import { PRODUCTS } from "@/products.exemple";
 import { Separator } from "@/src/components/ui/separator";
 import ProductWithOptionsCard from "@/src/blocks/products/ProductWithOptionsCard";
-import { CBD_FLOWERS } from "@/productsFleursCBD.exemple";
 import SortSelector from "@/src/blocks/sortSelector/SortSelector";
+import { PRODUCTS, PRODUCTS_CATEGORIES } from "@/mock";
+import Link from "next/link";
 
 const sortOptions = [
   "Alphabetique (A → Z)",
@@ -19,26 +18,24 @@ const sortOptions = [
 
 const PageProduits = () => {
   const [sortBy, setSortBy] = useState<string>(sortOptions[0]);
-
   // On memoïse le tri pour ne pas recalculer à chaque render
   const sortedProducts = useMemo(() => {
     // Clone l’array pour ne pas muter l’original
-    const arr = [...CBD_FLOWERS];
     switch (sortBy) {
       case "Alphabetique (A → Z)":
-        return arr.sort((a, b) =>
+        return PRODUCTS.sort((a, b) =>
           a.name.localeCompare(b.name, "fr", { sensitivity: "base" })
         );
       case "Alphabetique (A ← Z)":
-        return arr.sort((a, b) =>
+        return PRODUCTS.sort((a, b) =>
           b.name.localeCompare(a.name, "fr", { sensitivity: "base" })
         );
       case "Prix croissant":
-        return arr.sort((a, b) => a.price! - b.price!);
+        return PRODUCTS.sort((a, b) => a.price! - b.price!);
       case "Prix décroissant":
-        return arr.sort((a, b) => b.price! - a.price!);
+        return PRODUCTS.sort((a, b) => b.price! - a.price!);
       default:
-        return arr;
+        return PRODUCTS;
     }
   }, [sortBy]);
   
@@ -46,6 +43,17 @@ const PageProduits = () => {
     <section id="contact-page" className="flex">
       <div className="flex-1 max-w-50">
         <h2 className="text-xl font-bold">Filtre</h2>
+        {PRODUCTS_CATEGORIES.map((category) => (
+          <div key={category.name} className="flex items-center gap-2 my-4">
+              <Link
+                href={`/produits/${category.name.toLocaleLowerCase()}`}
+                className="text-sm text-muted-foreground hover:text-primary"
+              >
+                {category.name}
+              </Link>
+            </div>
+        ))}
+            
       </div>
       <Separator orientation="vertical" className="mx-4" />
       <div className="flex-1">
