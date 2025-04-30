e677eb34faefd3ff6d9edd46353dc50bd9ffb993
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-04-30 23:06:41 +0200

    feat: intégrer les composants TrustBanner et CategoriesMenuBanner dans HeroCarousel

--- src/blocks/Hero/HeroCarousel.tsx
+++ src/blocks/Hero/HeroCarousel.tsx
@@ -7,6 +7,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/src/components/ui/carousel";
+import TrustBanner from "../banner/TrustBanner";
+import CategoriesMenuBanner from "../banner/MenuBanner";
 
 const pictures = [
   "https://plus.unsplash.com/premium_photo-1724593825200-39731dcdacf8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
@@ -23,22 +25,26 @@ const pictures = [
 
 const HeroCarousel: React.FC = () => {
   return (
-    <Carousel className="w-full" loop itemsToShow={3} autoplay={5000}>
-      <CarouselContent>
-        {pictures.map((src, index) => (
-          <CarouselItem key={index}>
-            <img
-              src={src}
-              alt={`Image ${index + 1}`}
-              className="w-full aspect-square object-cover"
-              loading="lazy"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/* <CarouselPrevious /> */}
-      {/* <CarouselNext /> */}
-    </Carousel>
+    <section className="relative">
+      <CategoriesMenuBanner className="my-8"/>
+      <Carousel className="w-full rounded-2xl overflow-hidden" loop itemsToShow={3} autoplay={5000}>
+        <CarouselContent>
+          {pictures.map((src, index) => (
+            <CarouselItem key={index}>
+              <img
+                src={src}
+                alt={`Image ${index + 1}`}
+                className="w-full aspect-square object-cover"
+                loading="lazy"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/* <CarouselPrevious /> */}
+        {/* <CarouselNext /> */}
+      </Carousel>
+      <TrustBanner className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"/>
+    </section>
   );
 };
 
