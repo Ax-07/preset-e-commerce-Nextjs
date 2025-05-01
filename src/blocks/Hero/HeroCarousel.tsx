e401b6dbba3a410fa8e6ef9d08a965629b14e268
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-01 18:04:38 +0200

    feat: mettre à jour les images du carrousel et ajuster la durée de l'autoplay

--- src/blocks/Hero/HeroCarousel.tsx
+++ src/blocks/Hero/HeroCarousel.tsx
@@ -11,23 +11,20 @@ import TrustBanner from "../banner/TrustBanner";
 import CategoriesMenuBanner from "../banner/MenuBanner";
 
 const pictures = [
-  "https://plus.unsplash.com/premium_photo-1724593825200-39731dcdacf8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
-  "https://plus.unsplash.com/premium_photo-1682629430351-2cbf66cff813?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDh8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1743865422216-d8a188311ab9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://plus.unsplash.com/premium_photo-1716832318033-30fe0671866b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1744273556970-d5cff25b2b4a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1744132813635-128ed5004f65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1743856842985-e1d4fc72a255?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1743660624046-8e914498fb4d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1743485753906-f190eb90e300?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ5fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1697172812290-6543a011c3ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDY0fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
+  "https://images.unsplash.com/flagged/photo-1564833125683-b9fde4516274?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2JkfGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1604660664082-3cac347079b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2JkfGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1616690002494-c8cbe7735682?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNiZHxlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1563660322566-e9bc3441b70c?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1602168431602-9bfa3722a472?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1525575674331-70638740994c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fGNiZHxlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1605039828994-9ad00c0cc10c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNiZHxlbnwwfHwwfHx8MA%3D%3D",
 ];
 
 const HeroCarousel: React.FC = () => {
   return (
     <section className="relative">
       <CategoriesMenuBanner className="my-8"/>
-      <Carousel className="w-full rounded-2xl overflow-hidden" loop itemsToShow={3} autoplay={5000}>
+      <Carousel className="w-full rounded-2xl overflow-hidden" loop itemsToShow={3} autoplay={4000}>
         <CarouselContent>
           {pictures.map((src, index) => (
             <CarouselItem key={index}>
