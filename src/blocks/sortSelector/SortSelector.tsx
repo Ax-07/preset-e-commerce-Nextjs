239b24fae61b50648ccc8223aef8e654d3d4a1ba
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-01 18:04:53 +0200

    refactor: mettre à jour le type des options dans le sélecteur de tri et simplifier le rendu des éléments

--- src/blocks/sortSelector/SortSelector.tsx
+++ src/blocks/sortSelector/SortSelector.tsx
@@ -8,20 +8,22 @@ import {
     SelectValue,
   } from "@/src/components/ui/select";
   import React from "react";
+import { SortOption } from "./useSort";
   
   interface SortSelectorProps {
-    options: string[];
+    options: SortOption<any>[];
     onChange: (value: string) => void;
   }
   
   const SortSelector: React.FC<SortSelectorProps> = ({ options, onChange }) => {
+    const label = options.map(o => o.label)
     return (
       <Select onValueChange={onChange}>
         <SelectTrigger className="w-[180px]">
           <SelectValue placeholder="Trier par…" />
         </SelectTrigger>
         <SelectContent>
-          {options.map((opt) => (
+          {label.map((opt) => (
             <SelectItem key={opt} value={opt}>
               {opt}
             </SelectItem>
