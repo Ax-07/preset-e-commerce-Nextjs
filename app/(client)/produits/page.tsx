f34290b7853933764e135152d2d67092c03f5a62
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-01 18:04:13 +0200

    feat: refactor la logique de tri des produits en utilisant un hook personnalisé

--- app/(client)/produits/page.tsx
+++ app/(client)/produits/page.tsx
@@ -2,42 +2,18 @@
 // @/src/app/produits/page.tsx
 
 import { List, ListItem } from "@/src/blocks/products/List";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { Separator } from "@/src/components/ui/separator";
 import ProductWithOptionsCard from "@/src/blocks/products/ProductWithOptionsCard";
-import SortSelector from "@/src/blocks/sortSelector/SortSelector";
 import { PRODUCTS, PRODUCTS_CATEGORIES } from "@/mock";
 import Link from "next/link";
-
-const sortOptions = [
-  "Alphabetique (A → Z)",
-  "Alphabetique (A ← Z)",
-  "Prix croissant",
-  "Prix décroissant",
-];
+import { productSortOptions, SortSelector, useSort } from "@/src/blocks/sortSelector";
 
 const PageProduits = () => {
-  const [sortBy, setSortBy] = useState<string>(sortOptions[0]);
-  // On memoïse le tri pour ne pas recalculer à chaque render
-  const sortedProducts = useMemo(() => {
-    // Clone l’array pour ne pas muter l’original
-    switch (sortBy) {
-      case "Alphabetique (A → Z)":
-        return PRODUCTS.sort((a, b) =>
-          a.name.localeCompare(b.name, "fr", { sensitivity: "base" })
-        );
-      case "Alphabetique (A ← Z)":
-        return PRODUCTS.sort((a, b) =>
-          b.name.localeCompare(a.name, "fr", { sensitivity: "base" })
-        );
-      case "Prix croissant":
-        return PRODUCTS.sort((a, b) => a.price! - b.price!);
-      case "Prix décroissant":
-        return PRODUCTS.sort((a, b) => b.price! - a.price!);
-      default:
-        return PRODUCTS;
-    }
-  }, [sortBy]);
+    const { sortedItems, setSortBy } = useSort({
+      items: PRODUCTS,
+      options: productSortOptions,
+    });
   
   return (
     <section id="contact-page" className="flex">
@@ -59,10 +35,10 @@ const PageProduits = () => {
       <div className="flex-1">
         <div className="flex justify-between">
           <h2 className="text-xl font-bold">Tous les Produits</h2>
-          <SortSelector options={sortOptions} onChange={setSortBy}/>
+          <SortSelector options={productSortOptions} onChange={setSortBy}/>
         </div>
         <List layout="grid" gap="md" cols="4" className="mt-8">
-          {sortedProducts.map((product) => (
+          {sortedItems.map((product) => (
             <ListItem key={product.id}>
               {/* <ProductCard product={product} /> */}
               <ProductWithOptionsCard product={product} />
