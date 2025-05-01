7e50735c1740821dddf848b5793dad0d7ea5d0d4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-01 18:04:34 +0200

    feat: améliorer le menu des catégories avec gestion du survol et affichage des médias

--- src/blocks/banner/MenuBanner.tsx
+++ src/blocks/banner/MenuBanner.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -5,9 +7,8 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/src/components/ui/navigation-menu";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ListItem } from "../Navbar/ListItem";
-import { navigationConfig } from "@/config/navigation.config";
 import { cn } from "@/src/utils/tailwind_cn";
 import { PRODUCTS_CATEGORIES_WITH_SUBCATEGORIES } from "@/mock";
 import { slugify } from "@/src/utils/slug";
@@ -18,60 +19,39 @@ interface CategoriesMenuBannerProps {
 const CategoriesMenuBanner: React.FC<CategoriesMenuBannerProps> = ({
   className,
 }) => {
+  const [hovered, setHovered] = useState<{ name: string; image?: string } | null>(null);
+
   return (
     <NavigationMenu className={cn("max-w-8/10 mx-auto w-full z-9", className)}>
       <NavigationMenuList className="bg-primary/50 py-2 px-10 md:flex hidden md:flex-row items-center justify-between gap-8 rounded-2xl overflow-hidden">
-        {/* {navigationConfig.categoriesNav.map((group, index) => (
-          <NavigationMenuItem key={index} className="bg-transparent">
-            <NavigationMenuTrigger className="bg-transparent text-foreground flex-1">
-              {group.title}
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="left-0 top-0 right-0 w-full flex-1 z-100">
-              <ul className="flex p-3">
-                <aside className="w-50 bg-primary-foreground p-2 rounded-md mr-4"></aside>
-                <div className="w-full grid grid-cols-2 gap-4">
-                  {group.subMenuItems.map((item, idx) => (
-                    <div key={idx} className="flex flex-col gap-2">
-                      <ListItem
-                        key={idx}
-                        title={item.title}
-                        href={item.link}
-                        icon={item.icon}
-                      >
-                        {item.description}
-                      </ListItem>
-                      <ul className="flex flex-col gap-2 pl-7">
-                        {item.subMenuItems.map((subItem, subIdx) => (
-                          <ListItem
-                            key={subIdx}
-                            title={subItem.title}
-                            href={subItem.link}
-                          >
-                            {subItem.description}
-                          </ListItem>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        ))} */}
         {PRODUCTS_CATEGORIES_WITH_SUBCATEGORIES.map((category, index) => (
           <NavigationMenuItem key={index} className="bg-transparent">
-            <NavigationMenuTrigger className="bg-transparent text-foreground flex-1">
+            <NavigationMenuTrigger className="bg-transparent text-foreground flex-1"
+            onMouseEnter={() => setHovered({
+              name: category.name,
+              image: category.media?.url,
+            })}
+            >
               {category.name}
             </NavigationMenuTrigger>
             <NavigationMenuContent className="left-0 top-0 right-0 w-full flex-1 z-100">
               <ul className="flex p-3">
-                <aside className="w-50 bg-primary-foreground p-2 rounded-md mr-4"></aside>
+                <aside className={cn("w-48 h-48 rounded-lg overflow-hidden mr-4 transition-all duration-200" )}
+                >
+                  <img src={hovered?.image} alt="" className="w-full h-full object-cover" loading="lazy"/>
+                </aside>
                 <div className="w-full grid grid-cols-2 gap-4">
                   {category.subcategories?.map((subCategory, idx) => (
                     <ListItem
                       key={idx}
                       title={subCategory.name}
                       href={`/produits/${slugify(category.name.toLowerCase())}/${slugify(subCategory.name.toLocaleLowerCase())}`}
+                      onMouseEnter={() =>
+                        setHovered({
+                          name: subCategory.name,
+                          image: subCategory.media?.url,
+                        })
+                      }
                     >
                       {subCategory.description}
                     </ListItem>
@@ -81,7 +61,6 @@ const CategoriesMenuBanner: React.FC<CategoriesMenuBannerProps> = ({
             </NavigationMenuContent>
           </NavigationMenuItem>
         ))}
-        
       </NavigationMenuList>
     </NavigationMenu>
   );
