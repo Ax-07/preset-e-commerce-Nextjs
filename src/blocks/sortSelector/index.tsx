7d64e52aa41547a6d6563f283dfe298a984248c4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-01 18:04:43 +0200

    feat: ajouter le sélecteur de tri des produits

---
+++ src/blocks/sortSelector/index.tsx
@@ -0,0 +1,6 @@
+import { useSort } from "./useSort";
+import SortSelector from "./SortSelector";
+import { productSortOptions } from "./sortOptions";
+
+export { useSort, SortSelector, productSortOptions };
+export type { SortOption } from "./useSort";
