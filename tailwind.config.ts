17285ce26f767193e049bb90304435b0295b0270
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-03 20:50:28 +0200

    feat: étendre la configuration de Tailwind pour inclure des animations de texte

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	theme: {
-	  container: {
-		center: true,
-		padding: {
-		  DEFAULT: "1rem",
-		  sm: "2rem",
-		  lg: "4rem",
-		  xl: "5rem",
-		},
-		screen: {
-		  sm: "640px",
-		  md: "768px",
-		  lg: "1024px",
-		  xl: "1280px",
-		  '2xl': "1536px",
-		},
-	  },
-	},
-  };
-  
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+      screen: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            transform: "translate(-50%, 0%)",
+            opacity: 0,
+            filter: "blur(0px)",
+            background: "rgba(255, 255, 255, 0)",
+          },
+          "75%": {
+            filter: "blur(20px)",
+            background: "rgba(255, 255, 255, 1.0)",
+          },
+          "100%": {
+            position: "relative",
+            transform: "translate(0, 0)",
+            opacity: 1,
+            filter: "blur(0px)",
+            background: "rgba(255, 255, 255, 0)",
+          },
+        },
+      },
+    },
+  },
+};
