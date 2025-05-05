4706e4db2d8d6b3dab8b7e0aa5278eb2030c9ff4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 09:57:09 +0200

    refactor: remplacer NextApiRequest et NextApiResponse par NextResponse dans le gestionnaire de paiement

--- app/api/payment-status/route.ts
+++ app/api/payment-status/route.ts
@@ -1,27 +1,28 @@
 // src/pages/api/payment-status.ts
 
 import { stripe } from "@/src/lib/stripe/stripe.config";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).end("Method not allowed");
-  }
-
-  const { clientSecret } = req.body;
-
-  if (!clientSecret) {
-    return res.status(400).json({ error: "Client secret missing" });
-  }
-
+export default async function Post(req: Request) {
   try {
+    const { clientSecret } = await req.json();
+    if (!clientSecret) {
+      return NextResponse.json(
+        { error: "Client secret is required" },
+        { status: 400 }
+      );
+    }
     const paymentIntent = await stripe.paymentIntents.retrieve(
       clientSecret.split("_secret")[0]
     );
-
-    return res.status(200).json({ status: paymentIntent.status });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error" });
+    if (!paymentIntent) {
+      return NextResponse.json(
+        { error: "PaymentIntent not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ status: paymentIntent.status });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
